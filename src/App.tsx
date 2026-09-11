b724c2b95a2e97bb790e21b0923c6bb3fae5ef34
gptgo52-creator <gptgo5.2@gmail.com>
2026-09-11 12:57:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import ...
 import { HeroSection } from './components/HeroSection';
 import { AboutSection } from './components/AboutSection';
 import { ProjectsSection } from './components/ProjectsSection';
